59a236f33a6cfe4c8a5cde9a53bcc4459e62282d
Sofus Albert Høgsbro Rose <sofus@sofusrose.com>
2024-03-14 13:37:30 +0100

    packaging: Initialize virtual 'rye' project.

--- blender_maxwell/node_trees/maxwell_sim_nodes/contracts/schemas/node.py
+++
@@ -1,9 +0,0 @@
-import typing as typ
-
-import pydantic as pyd
-
-from ..bl import ManagedObjName, SocketName
-from ..managed_obj_type import ManagedObjType
-
-class MaxwellSimNode(typ.Protocol):
-	
